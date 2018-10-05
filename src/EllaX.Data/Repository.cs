439ab6c2206a7ac9110c13fe348729f12e6885e2
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-04 21:29:34 -0700

    add data project

---
+++ src/EllaX.Data/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using LiteDB;
+
+namespace EllaX.Data
+{
+    public class Repository : LiteRepository
+    {
+        public Repository(LiteDatabase database, bool disposeDatabase = false) : base(database, disposeDatabase) { }
+        public Repository(string connectionString, BsonMapper mapper = null) : base(connectionString, mapper) { }
+
+        public Repository(ConnectionString connectionString, BsonMapper mapper = null) :
+            base(connectionString, mapper) { }
+
+        public Repository(Stream stream, BsonMapper mapper = null, string password = null) : base(stream, mapper,
+            password) { }
+    }
+}
