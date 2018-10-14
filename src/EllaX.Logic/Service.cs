f4991f91952c60309f0672214977d07ae63ccc54
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-14 14:31:43 -0700

    add base client and service impl

---
+++ src/EllaX.Logic/Service.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace EllaX.Logic
+{
+    public abstract class Service
+    {
+        protected readonly IMediator EventBus;
+
+        protected Service(IMediator eventBus)
+        {
+            EventBus = eventBus;
+        }
+    }
+}
