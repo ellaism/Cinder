686253e450434f877a9be8dfd5e12295907a9cac
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-14 14:35:22 -0700

    remove indexing project

--- src/EllaX.Logic.Indexing/Exceptions/BlockNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EllaX.Logic.Indexing.Exceptions
-{
-    public class BlockNotFoundException : Exception
-    {
-        public BlockNotFoundException(long blockNumber) : base($"Block {blockNumber} returned null") { }
-    }
-}
--- src/EllaX.Logic.Indexing/IBlockProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace EllaX.Logic.Indexing
-{
-    public interface IBlockProcessor
-    {
-        Task ProcessBlockAsync(long blockNumber);
-    }
-}
