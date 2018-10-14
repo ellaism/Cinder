f47c74f50311da548d28be1667a459f05b3c5468
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-14 14:37:46 -0700

    peer notification object

---
+++ src/EllaX.Logic/Notifications/PeerNotification.cs
@@ -0,0 +1,10 @@
+﻿using EllaX.Core.Models;
+using MediatR;
+
+namespace EllaX.Logic.Notifications
+{
+    public class PeerNotification : INotification
+    {
+        public Peer Peer { get; set; }
+    }
+}
