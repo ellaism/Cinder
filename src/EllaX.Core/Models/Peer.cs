c748a396570077406ba53973f4aa005861d6d80b
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-14 14:36:51 -0700

    add peer model

---
+++ src/EllaX.Core/Models/Peer.cs
@@ -0,0 +1,10 @@
+﻿namespace EllaX.Core.Models
+{
+    public class Peer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LocalAddress { get; set; }
+        public string RemoteAddress { get; set; }
+    }
+}
