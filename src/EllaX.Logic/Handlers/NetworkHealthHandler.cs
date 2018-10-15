ffe0ebce4d99ae67b035d5c2b636e19cc7c25d57
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-14 22:09:19 -0700

    offload logic to service

---
+++ src/EllaX.Logic/Handlers/NetworkHealthHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EllaX.Logic.Notifications;
+using MediatR;
+
+namespace EllaX.Logic.Handlers
+{
+    public class NetworkHealthHandler : INotificationHandler<PeerNotification>
+    {
+        private readonly IPeerService _peerService;
+
+        public NetworkHealthHandler(IPeerService peerService)
+        {
+            _peerService = peerService;
+        }
+
+        public Task Handle(PeerNotification notification, CancellationToken cancellationToken)
+        {
+            return _peerService.ProcessPeerAsync(notification.Peer);
+        }
+    }
+}
