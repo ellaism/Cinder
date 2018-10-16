0882e5c23d12c2222131a314d5d414dfbb528c87
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-15 20:14:10 -0700

    add simple enumerable object mapper

---
+++ src/EllaX.Logic/Extensions/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
+
+namespace EllaX.Logic.Extensions
+{
+    public static class ListExtensions
+    {
+        public static IEnumerable<TDestination> MapTo<TDestination>(this IEnumerable sourceList, IMapper mapper)
+        {
+            return mapper.Map<IEnumerable<TDestination>>(sourceList);
+        }
+    }
+}
