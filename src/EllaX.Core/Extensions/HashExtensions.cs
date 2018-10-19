444c4bac0e453eea1208842cb56c95a625edd1fb
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-18 22:19:38 -0700

    add string extensions

---
+++ src/EllaX.Core/Extensions/HashExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EllaX.Core.Extensions
+{
+    public static class HashExtensions
+    {
+        public static string Md5(this string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+                byte[] hash = md5.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public static string Sha256(this string input)
+        {
+            using (SHA512 sha = SHA512.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+                byte[] hash = sha.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public static string Sha512(this string input)
+        {
+            using (SHA512 sha = SHA512.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+                byte[] hash = sha.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
