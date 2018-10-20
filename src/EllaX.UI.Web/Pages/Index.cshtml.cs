eed93165e0719a85e255ed168c738aff74b47c43
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-19 17:25:23 -0700

    some blazor work

---
+++ src/EllaX.UI.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using BlazorTourOfHeroes.Client.Services;
+using BlazorTourOfHeroes.Shared;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EllaX.UI.Web.Pages
+{
+    public class IndexModel : BlazorComponent
+    {
+
+    }
+}
