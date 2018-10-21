cd2983b95e3bb039eb85de55364ffe669a038d53
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-20 21:07:59 -0700

    scope dependencies

--- src/EllaX.Api/Infrastructure/Hosting/NetworkHealthHostedService.cs
+++ src/EllaX.Api/Infrastructure/Hosting/NetworkHealthHostedService.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using EllaX.Logic;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -9,16 +10,14 @@ namespace EllaX.Api.Infrastructure.Hosting
 {
     public class NetworkHealthHostedService : IHostedService
     {
-        private readonly IBlockchainService _blockchainService;
         private readonly ILogger<NetworkHealthHostedService> _logger;
-        private readonly IStatisticsService _statisticsService;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
         private DateTimeOffset _lastPeerCountSnapshot = DateTimeOffset.MinValue;
 
-        public NetworkHealthHostedService(IBlockchainService blockchainService, IStatisticsService statisticsService,
+        public NetworkHealthHostedService(IServiceScopeFactory serviceScopeFactory,
             ILogger<NetworkHealthHostedService> logger)
         {
-            _blockchainService = blockchainService;
-            _statisticsService = statisticsService;
+            _serviceScopeFactory = serviceScopeFactory;
             _logger = logger;
         }
 
@@ -27,15 +26,12 @@ namespace EllaX.Api.Infrastructure.Hosting
             _logger.LogInformation("Network health hosted service is starting");
             await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
 
-            while (!cancellationToken.IsCancellationRequested)
+            using (IServiceScope scope = _serviceScopeFactory.CreateScope())
             {
-                await CheckNetworkHealthAsync(cancellationToken);
+                IBlockchainService blockchainService = scope.ServiceProvider.GetService<IBlockchainService>();
+                IStatisticsService statisticsService = scope.ServiceProvider.GetService<IStatisticsService>();
 
-                if (_lastPeerCountSnapshot == DateTimeOffset.MinValue ||
-                    DateTimeOffset.UtcNow - _lastPeerCountSnapshot > TimeSpan.FromMinutes(60))
-                {
-                    await Snapshot(cancellationToken);
-                }
+                await DoWork(blockchainService, statisticsService, cancellationToken);
             }
         }
 
@@ -46,21 +42,37 @@ namespace EllaX.Api.Infrastructure.Hosting
             return Task.CompletedTask;
         }
 
-        private async Task Snapshot(CancellationToken cancellationToken)
+        private async Task DoWork(IBlockchainService blockchainService, IStatisticsService statisticsService,
+            CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await CheckNetworkHealthAsync(blockchainService, cancellationToken);
+
+                if (_lastPeerCountSnapshot == DateTimeOffset.MinValue ||
+                    DateTimeOffset.UtcNow - _lastPeerCountSnapshot > TimeSpan.FromMinutes(60))
+                {
+                    await Snapshot(statisticsService, cancellationToken);
+                }
+            }
+        }
+
+        private async Task Snapshot(IStatisticsService statisticsService, CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
             _logger.LogInformation("Taking snapshot of recent peers");
 
-            await _statisticsService.SnapshotRecentPeerCountAsync();
+            await statisticsService.SnapshotRecentPeerCountAsync();
             _lastPeerCountSnapshot = DateTimeOffset.UtcNow;
         }
 
-        private async Task CheckNetworkHealthAsync(CancellationToken cancellationToken)
+        private async Task CheckNetworkHealthAsync(IBlockchainService blockchainService,
+            CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
             _logger.LogInformation("Retrieving latest network health snapshot");
 
-            await _blockchainService.GetHealthAsync(cancellationToken);
+            await blockchainService.GetHealthAsync(cancellationToken);
             await Task.Delay(TimeSpan.FromSeconds(120), cancellationToken);
         }
     }
