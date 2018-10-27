8e5b4e50403cc1129ecf64f6e4a04dcb16e1308b
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-27 00:39:14 -0700

    add comments

--- src/EllaX.Api/Infrastructure/Extensions/EllaXContainerBuilderExtensions.cs
+++ src/EllaX.Api/Infrastructure/Extensions/EllaXContainerBuilderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using EllaX.Data;
+using EllaX.Indexing;
 using EllaX.Logic.Services;
 using EllaX.Logic.Services.Location;
 using EllaX.Logic.Services.Statistics;
@@ -10,12 +11,17 @@ namespace EllaX.Api.Infrastructure.Extensions
     {
         public static void RegisterEllaXTypes(this ContainerBuilder builder)
         {
+            // repository
             builder.RegisterType<Repository>().SingleInstance();
-            builder.RegisterType<LocationService>().As<ILocationService>().SingleInstance();
 
+            // services
+            builder.RegisterType<LocationService>().As<ILocationService>().SingleInstance();
             builder.RegisterType<BlockchainService>().As<IBlockchainService>().InstancePerLifetimeScope();
             builder.RegisterType<PeerService>().As<IPeerService>().InstancePerLifetimeScope();
             builder.RegisterType<StatisticsService>().As<IStatisticsService>().InstancePerLifetimeScope();
+
+            // indexer
+            builder.RegisterType<Indexer>().As<IIndexer>().InstancePerLifetimeScope();
         }
     }
 }
