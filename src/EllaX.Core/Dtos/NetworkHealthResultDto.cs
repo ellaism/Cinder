305890856124dc5226cd70ee9e9c2b3c936f2a5f
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-26 20:09:25 -0700

    rename dto

---
+++ src/EllaX.Core/Dtos/NetworkHealthResultDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EllaX.Core.Dtos
+{
+    public class NetworkHealthResultDto
+    {
+        public int Count { get; set; }
+        public IReadOnlyCollection<PeerHealthDto> Peers { get; set; } = new PeerHealthDto[0];
+    }
+}
