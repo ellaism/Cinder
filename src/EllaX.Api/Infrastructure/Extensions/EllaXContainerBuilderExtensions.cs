cd5a91632f644195964e016b14f825fd6057f9c6
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-26 23:10:58 -0700

    relegate specific registrations to api project

---
+++ src/EllaX.Api/Infrastructure/Extensions/EllaXContainerBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using EllaX.Data;
+using EllaX.Logic.Services;
+using EllaX.Logic.Services.Location;
+using EllaX.Logic.Services.Statistics;
+
+namespace EllaX.Api.Infrastructure.Extensions
+{
+    public static class EllaXContainerBuilderExtensions
+    {
+        public static void RegisterEllaXTypes(this ContainerBuilder builder)
+        {
+            builder.RegisterType<Repository>().SingleInstance();
+            builder.RegisterType<LocationService>().As<ILocationService>().SingleInstance();
+
+            builder.RegisterType<BlockchainService>().As<IBlockchainService>().InstancePerLifetimeScope();
+            builder.RegisterType<PeerService>().As<IPeerService>().InstancePerLifetimeScope();
+            builder.RegisterType<StatisticsService>().As<IStatisticsService>().InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ src/EllaX.Api/Infrastructure/Extensions/EllaXServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using EllaX.Clients;
+using EllaX.Clients.Blockchain;
+using EllaX.Clients.Network;
+using EllaX.Data;
+using EllaX.Logic;
+using Microsoft.Extensions.Configuration;
+using Polly;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class EllaXServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEllaX(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.Configure<RepositoryOptions>(options =>
+                options.ConnectionString = configuration.GetConnectionString("RepositoryConnection"));
+            services.Configure<LocationOptions>(options =>
+                options.ConnectionString = configuration.GetConnectionString("GeoIpConnection"));
+            services.Configure<BlockchainClientOptions>(configuration.GetSection("EllaX:Blockchain"));
+            services.Configure<NetworkClientOptions>(configuration.GetSection("EllaX:Network"));
+
+            services.AddOptions();
+            services.AddHttpClient<IBlockchainClient, BlockchainClient>()
+                .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+            services.AddHttpClient<INetworkClient, NetworkClient>()
+                .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+
+            return services;
+        }
+    }
+}
