4fb9d2f94732e1f0e306e9a9b542b009441efa61
majordutch <354294+majordutch@users.noreply.github.com>
2018-10-29 22:52:42 -0700

    add logged exception

---
+++ src/EllaX.Core/Exceptions/LoggedException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace EllaX.Core.Exceptions
+{
+    public class LoggedException : AggregateException
+    {
+        public LoggedException() { }
+
+        public LoggedException(IEnumerable<Exception> innerExceptions) : base(innerExceptions) { }
+
+        public LoggedException(params Exception[] innerExceptions) : base(innerExceptions) { }
+
+        public LoggedException(string message) : base(message) { }
+
+        public LoggedException(string message, IEnumerable<Exception> innerExceptions) :
+            base(message, innerExceptions) { }
+
+        public LoggedException(string message, Exception innerException) : base(message, innerException) { }
+
+        public LoggedException(string message, params Exception[] innerExceptions) : base(message, innerExceptions) { }
+
+        protected LoggedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
