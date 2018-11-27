144c04a164e57bfb0306cdf0e6ff8d5954403e2b
majordutch <354294+majordutch@users.noreply.github.com>
2018-11-26 19:20:26 -0800

    move indexer-related registration to indexing project

---
+++ src/EllaX.Logic.Indexing/Extensions/ContainerBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using EllaX.Logic.Indexing;
+
+// ReSharper disable once CheckNamespace
+namespace EllaX.Mvc.Extensions
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static void RegisterEllaXIndexers(this ContainerBuilder builder)
+        {
+            // indexers
+            builder.RegisterType<BlockchainIndexer>().As<IIndexer>().InstancePerLifetimeScope();
+            builder.RegisterType<StatisticsIndexer>().As<IIndexer>().InstancePerLifetimeScope();
+            builder.RegisterType<IndexerManager>().As<IIndexerManager>().InstancePerLifetimeScope();
+        }
+    }
+}
