c7fe7ef7ff4a99c96dfad52271f90dbbb768bf7c
Ryan Jauregui <354294+majordutch@users.noreply.github.com>
2018-12-21 16:48:30 -0800

    add transaction logic

--- src/EllaX.Data/ApplicationDbContext.cs
+++ src/EllaX.Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
-﻿using EllaX.Core.Entities;
+﻿using System.Data;
+using System.Threading.Tasks;
+using EllaX.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace EllaX.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        private IDbContextTransaction _currentTransaction;
+
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Block> Blocks { get; set; }
@@ -13,6 +18,55 @@ namespace EllaX.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public void BeginTransaction()
+        {
+            if (_currentTransaction != null)
+            {
+                return;
+            }
+
+            _currentTransaction = Database.BeginTransaction(IsolationLevel.ReadCommitted);
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await SaveChangesAsync();
+
+                _currentTransaction?.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
         }
     }
 }
