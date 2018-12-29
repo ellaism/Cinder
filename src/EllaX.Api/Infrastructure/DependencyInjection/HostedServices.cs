978154864c42805c777187404bdcde1a5755e588
Ryan Jauregui <354294+majordutch@users.noreply.github.com>
2018-12-28 18:12:30 -0800

    add hosted service for indexer

---
+++ src/EllaX.Api/Infrastructure/DependencyInjection/HostedServices.cs
@@ -0,0 +1,14 @@
+﻿using EllaX.Api.Infrastructure.HostedServices;
+using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable once CheckNamespace
+namespace EllaX.DependencyInjection
+{
+    public static class HostedServices
+    {
+        public static void AddHostedServices(this IServiceCollection services)
+        {
+            services.AddHostedService<IndexerHostedService>();
+        }
+    }
+}
---
+++ src/EllaX.Api/Infrastructure/HostedServices/IndexerHostedService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EllaX.Indexing;
+using Microsoft.Extensions.Hosting;
+
+namespace EllaX.Api.Infrastructure.HostedServices
+{
+    public class IndexerHostedService : BackgroundService
+    {
+        private readonly IIndexerManager _indexManager;
+
+        public IndexerHostedService(IIndexerManager indexManager)
+        {
+            _indexManager = indexManager;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _indexManager.RunAsync(stoppingToken);
+        }
+    }
+}
