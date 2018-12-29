044edbeb925d2830f5bb1826d112de0f7ef367cc
Ryan Jauregui <354294+majordutch@users.noreply.github.com>
2018-12-28 18:13:10 -0800

    add placeholder authorization policy

---
+++ src/EllaX.Api/Infrastructure/DependencyInjection/AuthorizationPolicies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using EllaX.Api.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+// ReSharper disable once CheckNamespace
+namespace EllaX.DependencyInjection
+{
+    public static class AuthorizationPolicies
+    {
+        public static void AddAuthorizationPolicies(this IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Management", policy => policy.RequireClaim("Manager"));
+            });
+        }
+    }
+}
