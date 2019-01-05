a36ddcbab270fd8b23dfc979b5fae352a3b34239
majordutch <354294+majordutch@users.noreply.github.com>
2019-01-05 09:55:55 -0800

    remove unused commands

--- src/EllaX.Api.Application/Features/Block/MappingProfile.cs
+++ src/EllaX.Api.Application/Features/Block/MappingProfile.cs
@@ -8,9 +8,6 @@ namespace EllaX.Api.Application.Features.Block
         {
             CreateMap<Core.Entities.Block, List.Model>();
             CreateMap<Core.Entities.Block, Detail.Model>();
-            CreateMap<Create.Command, Core.Entities.Block>(MemberList.Source);
-            CreateMap<Core.Entities.Block, Update.Command>().ReverseMap();
-            CreateMap<Core.Entities.Block, Delete.Command>();
         }
     }
 }
