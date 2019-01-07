45f27f63e4a9e20931fafa45f00679ee940c7728
majordutch <354294+majordutch@users.noreply.github.com>
2019-01-06 23:28:27 -0800

    additional resolvers

---
+++ src/EllaX.Clients/Resolvers/HexBigIntegerToDecimalResolver.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Nethereum.Hex.HexTypes;
+using Nethereum.Util;
+
+namespace EllaX.Clients.Resolvers
+{
+    public class HexBigIntegerToDecimalResolver : IMemberValueResolver<object, object, HexBigInteger, decimal>
+    {
+        public decimal Resolve(object source, object destination, HexBigInteger sourceMember, decimal destMember,
+            ResolutionContext context)
+        {
+            return UnitConversion.Convert.FromWei(sourceMember);
+        }
+    }
+}
