626915dfc3dc157fc5539b60f00e03bd6c1a8173
majordutch <354294+majordutch@users.noreply.github.com>
2019-08-17 16:53:51 -0700

    Wire services into DI

--- src/Cinder.UI/Infrastructure/Startup.cs
+++ src/Cinder.UI/Infrastructure/Startup.cs
@@ -1,5 +1,7 @@
+using Cinder.UI.Infrastructure.Clients;
 using Cinder.UI.Infrastructure.Hosting;
 using Cinder.UI.Infrastructure.Services;
+using Foundatio.Caching;
 using Foundatio.Messaging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -24,11 +26,12 @@ namespace Cinder.UI.Infrastructure
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<IMessageBus>(new InMemoryMessageBus());
+            services.AddSingleton<IMessageBus, InMemoryMessageBus>();
+            services.AddSingleton<ICacheClient, InMemoryCacheClient>();
             services.AddHostedService<StatsBackgroundService>();
-            services.AddHttpClient<ICinderApiService, CinderApiService>();
-            services.AddMemoryCache();
-            services.AddSingleton<IStatsService, StatsService>();
+            services.AddHttpClient<IApiClient, ApiClient>();
+            services.AddSingleton<IBlockService, BlockService>();
+            services.AddSingleton<ITransactionService, TransactionService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
