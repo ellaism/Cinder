aadaa9038a95cb5047a3d95ddceee8e770a31778
majordutch <354294+majordutch@users.noreply.github.com>
2019-08-17 16:54:15 -0700

    Add BlockDto

---
+++ src/Cinder.UI/Infrastructure/Dtos/BlockDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Cinder.UI.Infrastructure.Dtos
+{
+    public class BlockDto
+    {
+        public string BlockNumber { get; set; }
+        public string Hash { get; set; }
+        public string ParentHash { get; set; }
+        public string Nonce { get; set; }
+        public string ExtraData { get; set; }
+        public string Difficulty { get; set; }
+        public string TotalDifficulty { get; set; }
+        public ulong Size { get; set; }
+        public string Miner { get; set; }
+        public ulong GasLimit { get; set; }
+        public ulong GasUsed { get; set; }
+        public ulong Timestamp { get; set; }
+        public ulong TransactionCount { get; set; }
+        public string[] Uncles { get; set; }
+        public ulong UncleCount { get; set; }
+        public string Sha3Uncles { get; set; }
+    }
+}
