3e27a8ed49701f4ab52e0b63ca00692b0bfad8fe
majordutch <354294+majordutch@users.noreply.github.com>
2019-08-21 17:23:51 -0700

    Rename dev pool

--- src/Cinder.Api/Infrastructure/Services/MinerLookupService.cs
+++ src/Cinder.Api/Infrastructure/Services/MinerLookupService.cs
@@ -6,9 +6,7 @@ namespace Cinder.Api.Infrastructure.Services
     {
         private readonly Dictionary<string, string> _miners = new Dictionary<string, string>
         {
-            {"0x65767ec6d4d3d18a200842352485cdc37cbf3a21", "Ellaism Dev Mining Pool"}
-            //{"0x65767ec6d4d3d18a200842352485cdc37cbf3a21", "Pool Sexy"},
-            //{"0x65767ec6d4d3d18a200842352485cdc37cbf3a21", "Comining"}
+            {"0x65767ec6d4d3d18a200842352485cdc37cbf3a21", "Ellaism Dev Pool"}
         };
 
         public string GetByAddressOrDefault(string hash)
