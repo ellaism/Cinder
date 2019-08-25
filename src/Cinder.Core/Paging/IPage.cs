83f3ef78740d33d27f3ce1234aded47f065abb84
majordutch <354294+majordutch@users.noreply.github.com>
2019-08-24 21:29:37 -0700

    Add pagination logic

---
+++ src/Cinder.Core/Paging/IPage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Cinder.Core.Paging
+{
+    public interface IPage<out T>
+    {
+        int Page { get; }
+        int Size { get; }
+        int Total { get; }
+        IEnumerable<T> Items { get; }
+    }
+}
---
+++ src/Cinder.Core/Paging/PagedEnumerable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Cinder.Core.Paging
+{
+    public class PagedEnumerable<T> : IPage<T>
+    {
+        public PagedEnumerable(IEnumerable<T> items, int total, int page, int size)
+        {
+            Page = page;
+            Size = size;
+            Total = total;
+            Items = items;
+        }
+
+        public int Page { get; }
+        public int Size { get; }
+        public int Total { get; }
+        public IEnumerable<T> Items { get; }
+    }
+}
