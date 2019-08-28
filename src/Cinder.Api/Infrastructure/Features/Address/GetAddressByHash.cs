e70afa47942df2bafe9764e7b00f0712d07b7656
majordutch <354294+majordutch@users.noreply.github.com>
2019-08-27 23:22:50 -0700

    Defer aggregates to another process

--- src/Cinder.Api/Infrastructure/Features/Address/GetAddressByHash.cs
+++ src/Cinder.Api/Infrastructure/Features/Address/GetAddressByHash.cs
@@ -30,24 +30,19 @@ namespace Cinder.Api.Infrastructure.Features.Address
         {
             public string Hash { get; set; }
             public decimal Balance { get; set; }
-            public ulong BlocksMined { get; set; }
-            public ulong TransactionCount { get; set; }
+            public ulong? BlocksMined { get; set; }
+            public ulong? TransactionCount { get; set; }
             public DateTimeOffset CacheDate { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, Model>
         {
             private readonly IAddressRepository _addressRepository;
-            private readonly IBlockRepository _blockRepository;
-            private readonly ITransactionRepository _transactionRepository;
             private readonly IWeb3Parity _web3;
 
-            public Handler(IAddressRepository addressRepository, IBlockRepository blockRepository,
-                ITransactionRepository transactionRepository, IWeb3Parity web3)
+            public Handler(IAddressRepository addressRepository, IWeb3Parity web3)
             {
                 _addressRepository = addressRepository;
-                _blockRepository = blockRepository;
-                _transactionRepository = transactionRepository;
                 _web3 = web3;
             }
 
@@ -72,10 +67,8 @@ namespace Cinder.Api.Infrastructure.Features.Address
                 {
                     Hash = request.Hash,
                     Balance = UnitConversion.Convert.FromWei(balance),
-                    BlocksMined = await _blockRepository.GetBlocksMinedCountByAddressHash(request.Hash, cancellationToken),
-                    TransactionCount =
-                        await _transactionRepository.GetTransactionCountByAddressHash(request.Hash, cancellationToken),
-                    CacheDate = DateTimeOffset.UtcNow
+                    CacheDate = DateTimeOffset.UtcNow,
+                    ForceRefresh = true
                 };
                 await _addressRepository.UpsertAddress(address, cancellationToken);
 
