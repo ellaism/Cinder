bfd12d03d04d3ac01ffdf3743f62af9af9f20e51
majordutch <354294+majordutch@users.noreply.github.com>
2019-08-27 23:24:48 -0700

    Version bump

--- src/Cinder.Core/VersionInfo.cs
+++ src/Cinder.Core/VersionInfo.cs
@@ -5,7 +5,7 @@ namespace Cinder.Core
 {
     public static class VersionInfo
     {
-        public const string Version = "1.0.19237.1";
+        public const string Version = "1.0.19239.1";
         public static DateTime BuildDate => new FileInfo(typeof(VersionInfo).Assembly.Location).LastWriteTime;
     }
 }
